d8123ae2c57344da285dbeadd21a57fc4ae917c4
Andres Tangarife <andresfelta95@gmail.com>
2023-03-11 12:37:48 -0700

    Obtained a json from a API with Pokemons info

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,22 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import {SafeAreaView, Text, } from 'react-native';
+import { getPokemon } from "../api/pokemon";
 
 export default function Pokemon() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemon();
+    })();
+  }, []);
+
+  const  loadPokemon = async () => {
+    try {
+      const response = await getPokemon();
+      console.log('response', response);
+    } catch (error) {
+      console.log('loadPokemon', error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Pokedex Page 🧑🏽‍💻</Text>
