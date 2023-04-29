e85b49d2b40244ddf25c1605fb92a9d6779a8815
Andres Tangarife <andresfelta95@gmail.com>
2023-04-29 07:35:18 -0600

    obtaining information for each pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+//  Get the pokemon details by id
+import { getPokemonDetailsByIdApi } from '../api/pokemon';
 
 export default function Pokemon(props) {
-  const { route } = props;
-  console.log(route);
+  const { navigation, route } = props;
+  const {id , name} = route.params;
+
+  const [pokemon, setPokemon] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsByIdApi(id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [route.params]);
+  console.log(pokemon);
+
+  if (!pokemon) return null;
+
   return (
     <SafeAreaView>
-      <Text>Pokemon Page 🧑🏽‍💻</Text>
+      <Text>Pokemon {pokemon.name}</Text>
     </SafeAreaView>
   );
 }
