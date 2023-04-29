f323785517b21ee0f09c02edbcfd4779ac8f9a87
Andres Tangarife <andresfelta95@gmail.com>
2023-04-28 20:38:09 -0600

    Give different styles depending the platform

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator, Platform } from 'react-native';
 import PokemonCard from "./PokemoCard";
 
 export default function PokemonList(props) {
@@ -36,9 +36,10 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
     flatListContainer: {
         paddingHorizontal: 5,
+        paddingTop: Platform.OS === 'android' ? 30 : 0,
     },
     spinner: {
         marginTop: 20,
-        marginBottom: 60,
+        marginBottom: Platform.OS === 'android' ? 90 : 60,
     },
 });
