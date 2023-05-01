41bf6087e68efbb710c985e22917fd7362d1ca8f
Andres Tangarife <andresfelta95@gmail.com>
2023-05-01 15:59:09 -0600

    Favorite icon updating on state

--- src/api/favorite.js
+++ src/api/favorite.js
@@ -2,6 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Key to store favorite pokemon
 import { FAVORITE_STORAGE } from "../utils/constants";
+import { includes } from "lodash";
 
 // Add a pokemon to favorite list
 export async function addPokemonToFavoriteApi(pokemonId) {
@@ -11,37 +12,32 @@ export async function addPokemonToFavoriteApi(pokemonId) {
 
     // If there is no favorite pokemon list
     if (!favoritePokemons) {
-        // Create a new list with the new pokemon
+        // Create a new list
         await AsyncStorage.setItem(
             FAVORITE_STORAGE,
             JSON.stringify([pokemonId])
         );
         return;
-        }
-
-    // If there is a favorite pokemon list
-    // Convert the list to an array
-    const favoritePokemonsArray = JSON.parse(favoritePokemons);
-
-    // Check if the pokemon is already in the list
-    const index = favoritePokemonsArray.indexOf(pokemonId);
-
-    // If the pokemon is already in the list
-    if (index > -1) {
-        // Remove the pokemon from the list
-        favoritePokemonsArray.splice(index, 1);
     }
+
+    // Check if the pokemon is in the list
+    const isPokemon = await isPokemonFavoriteApi(pokemonId);
+
     // If the pokemon is not in the list
-    else {
+    if (!isPokemon) {
         // Add the pokemon to the list
-        favoritePokemonsArray.push(pokemonId);
+        favoritePokemons.push(pokemonId);
+        // Update the list
+        await AsyncStorage.setItem(
+            FAVORITE_STORAGE,
+            JSON.stringify(favoritePokemons)
+        );
     }
-
-    // Save the new list
-    await AsyncStorage.setItem(
-        FAVORITE_STORAGE,
-        JSON.stringify(favoritePokemonsArray)
-    );
+    // If the pokemon is in the list
+    else {
+        // Remove the pokemon from the list
+        await removePokemonFromFavoriteApi(pokemonId);
+    }
   } catch (error) {
     console.log(error);
     throw error;
@@ -52,7 +48,61 @@ export async function addPokemonToFavoriteApi(pokemonId) {
 export async function getFavoritePokemonsApi() {
     try {
         const response = await AsyncStorage.getItem(FAVORITE_STORAGE);
-        return response;
+        return JSON.parse(response || []);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+// Check if a pokemon is in the favorite list
+export async function isPokemonFavoriteApi(pokemonId) {
+    try {
+        // Get favorite pokemon list
+        const favoritePokemons = await getFavoritePokemonsApi();
+
+        // If there is no favorite pokemon list
+        if (!favoritePokemons) {
+            return false;
+        }
+
+        // If there is a favorite pokemon list
+        return includes(favoritePokemons, pokemonId);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+// Remove a pokemon from the favorite list
+export async function removePokemonFromFavoriteApi(pokemonId) {
+    try {
+        // Get favorite pokemon list
+        const favoritePokemons = await getFavoritePokemonsApi();
+
+        // If there is no favorite pokemon list
+        if (!favoritePokemons) {
+            return;
+        }
+
+        // Check if the pokemon is in the list
+        const index = favoritePokemons.indexOf(pokemonId);
+
+        // If the pokemon is in the list
+        if (index > -1) {
+            // Remove the pokemon from the list
+            favoritePokemons.splice(index, 1);
+        }
+        // If the pokemon is not in the list
+        else {
+            return;
+        }
+
+        // Save the new list
+        await AsyncStorage.setItem(
+            FAVORITE_STORAGE,
+            JSON.stringify(favoritePokemons)
+        );
     } catch (error) {
         console.log(error);
         throw error;
--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,22 +1,52 @@
 import * as React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 // Api
-import { addPokemonToFavoriteApi, getFavoritePokemonsApi } from '../../api/favorite';
-import { View } from 'react-native';
+import { addPokemonToFavoriteApi, isPokemonFavoriteApi, removePokemonFromFavoriteApi } from '../../api/favorite';
 
 export default function Favorite(props) {
     const { id } = props;
+    const [isfavorite, setIsFavorite] = React.useState(false);
+    const [color, setColor] = React.useState('#fff');
+
+
     const addFavorite = async () => {
+        // Add the pokemon to the favorite list
         await addPokemonToFavoriteApi(id);
-        console.log('Pokemon added to favorite');
+        // Check if the pokemon is already in the favorite list
+        const response = await isPokemonFavoriteApi(id);
+        setIsFavorite(response);
     }
+
+    React.useEffect(() => {
+        (async () => {
+            try {
+                // Check if the pokemon is already in the favorite list
+                const response = await isPokemonFavoriteApi(id);
+                setIsFavorite(response);
+                // If the pokemon is already in the favorite list
+                if (response) {
+                    // Set the heart color to red
+                    setColor('#F44336');
+                }
+                // If the pokemon is not in the favorite list
+                else {
+                    // Set the heart color to white
+                    setColor('#fff');
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        })()
+    }, [id, isfavorite])
+
     return (
         <Icon
         name="heart"
-        color='#fff'
-        size={30}
+        color={color}
+        size={40}
         onPress={addFavorite}
-        style={{ marginRight: 20 }}
+        style={{ marginRight: 40 }}
         />
     );
 }
